61a9213f98c6d0947360af2c06d9504716f7ebc9
Digvijay Panwar <panwardigvijay17@gmail.com>
2022-02-12 17:27:58 +0530

    modifying Search page

--- src/pages/search/Search.js
+++ src/pages/search/Search.js
@@ -1,5 +1,23 @@
+import { useLocation } from "react-router-dom";
+import { useFetch } from "../../hooks/useFetch";
+import RecipeList from "../../components/RecipeList/RecipeList";
 import "./Search.css";
 function Search() {
-    return <div>Search</div>;
+    const queryString = useLocation().search;
+    const queryParams = new URLSearchParams(queryString);
+    const query = queryParams.get("q");
+
+    console.log(queryString);
+    const { data, isPending, error } = useFetch(
+        "http://localhost:3000/recipes?q=" + query
+    );
+    return (
+        <div>
+            <h2 className="page-title">Recipes including "{query}"</h2>
+            {error && <p className="error">{error}</p>}
+            {isPending && <p className="loading">Loading...</p>}
+            {data && <RecipeList recipes={data} />}
+        </div>
+    );
 }
 export default Search;
