995c6490a9ede8634d66c733847650d296d7c5db
Digvijay Panwar <panwardigvijay17@gmail.com>
2022-02-12 12:41:08 +0530

    Routes set

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import './App.css'
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import "./App.css";
+import Create from "./pages/Create/Create";
+import Home from "./pages/Home/Home";
+import Recipe from "./pages/Recipe/Recipe";
+import Search from "./pages/Search/Search";
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path="/">
+                        <Home />
+                    </Route>
+                    <Route path="/create">
+                        <Create />
+                    </Route>
+                    <Route path="/search">
+                        <Search />
+                    </Route>
+                    <Route path="/recipe/:id">
+                        <Recipe />
+                    </Route>
+                </Switch>
+            </BrowserRouter>
+        </div>
+    );
 }
 
-export default App
+export default App;
